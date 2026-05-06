05f4747db445435e28bb4a65c3777a766f58a985
AlienLinh <nguyenngoclinh@hust.edu.vn>
2026-05-06 23:40:36 +0700

    ci/cd fe success

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,12 @@ import { AuthProvider } from "react-oidc-context";
 
 // Cấu hình chìa khóa kết nối với Cognito
 const cognitoAuthConfig = {
-  authority: "https://cognito-idp.ap-southeast-1.amazonaws.com/ap-southeast-1_2cuzm4vfB",
-  client_id: "766sg6l71idhfs4cap2n9vfi9c",
-  redirect_uri: "https://d292ky6tu5wuyr.cloudfront.net",
+  authority: process.env.REACT_APP_COGNITO_DOMAIN,
+  client_id: process.env.REACT_APP_CLIENT_ID,
+  redirect_uri: process.env.REACT_APP_REDIRECT_URI,
   response_type: "code", // chuẩn OAuth 2.0 - sử dụng Authorization Code Flow
   scope: "email openid phone",
-};
+}; 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
